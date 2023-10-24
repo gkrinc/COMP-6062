712f37489b4d9b076d43febe2c739015ce9debd7
Greg Smith <greg.smith@gkr-inc.com>
2023-10-24 00:18:27 -0400

    useInteval

--- week 8/lab/traffic-lights/src/App.jsx
+++ week 8/lab/traffic-lights/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { useInterval } from './hooks';
 import TrafficLight from './TrafficLight';
 import './App.css'
 
 function App() {
   const [status, setStatus] = useState('stop');
+  const [delay, setDelay] = useState();
 
   const advanceStatus = () => {
     if (status === 'stop') {
@@ -15,6 +17,18 @@ function App() {
     }
   }
 
+  const toggleAutoAdvance = () => {
+    if (delay) {
+      setDelay(null);
+      return;
+    }
+    setDelay(2000);
+  };
+
+  useInterval(() => {
+    advanceStatus();
+  }, delay);
+
   return (
     <>
       <div className="mb-20">
@@ -23,6 +37,7 @@ function App() {
         <TrafficLight color="green" on={status === 'go'} />
       </div>
       <button onClick={() => { advanceStatus() }}>Advance</button>
+      <button onClick={() => toggleAutoAdvance() }>{delay ? 'Stop' : 'Auto'}</button>
     </>
   )
 }
