dfd6f6234a7f04c5b3794f0f5c6a22622a4eda89
Greg Smith <greg.smith@gkr-inc.com>
2023-11-15 13:18:20 -0500

    promise based fs

--- final-project/api/server.js
+++ final-project/api/server.js
@@ -1,32 +1,66 @@
 const express = require('express');
-const fs = require('fs');
+const fsp = require('fs').promises;
 const app = express();
 const port = 5001;
+
+/*
+const logError = (err, req, res, next) => {
+  console.error(err);
+  next(err);
+};
+
+const handleError = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).send('An error occured');
+};
+*/
 
 // Middleware for parsing req bodies to JSON (req.body)
 app.use(express.json());
 
-app.get('/api/volume', (req, res) => {
-  return fs.readFile('data.json', (err, data) => {
-    const json = JSON.parse(data);
-    res.send({ volume: json.volume });
-  });
+app.get('/api/volume', (req, res, next) => {
+  fsp.readFile('data1.json')
+    .then((data) => {
+      const json = JSON.parse(data);
+      res.send({ volume: json.volume });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Data file not found or corrupt');
+    });
 });
 
 app.put('/api/volume', (req, res) => {
-  return fs.readFile('data.json', (err, data) => {
-    const json = JSON.parse(data);
-    json.volume = Number(req.body.volume);
-    fs.writeFile('data.json', JSON.stringify(json), () => {
-      res.send({ volume: Number(req.body.volume) });
-    });
-  });
+  fsp.readFile('data.json')
+    .then((data) => {
+      const json = JSON.parse(data);
+      json.volume = Number(req.body.volume);
+      fsp.writeFile('data1.json', JSON.stringify(json))
+        .then(() => {
+          res.send({ volume: Number(req.body.volume) });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Data file not found or corrupt');
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send('Data file not found or corrupt');
+      });
 });
 
 // Path params
 app.get('/api/songs/:id', (req, res) => {
   res.send(req.params.id);
 });
+
+/*
+app.use(logError);
+app.use(handleError);
+*/
 
 app.listen(port, () => {
   console.log(`Final project API listening on port ${port}`);
